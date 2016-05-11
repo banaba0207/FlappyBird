afec1d2471d102f822b25f5addf15aae69c4548f
Takuro Yamaguchi <takuro.yamaguchi@PK-22110116.local>
2016-05-11 19:16:51 +0900

    Add CharacterReader

---
+++ Classes/CharacterReader.cpp
@@ -0,0 +1,9 @@
+//
+//  CharacterReader.cpp
+//  FlappyBird
+//
+//  Created by Takuro Yamaguchi on 2016/05/11.
+//
+//
+
+#include "CharacterReader.hpp"
---
+++ Classes/CharacterReader.hpp
@@ -0,0 +1,14 @@
+//
+//  CharacterReader.hpp
+//  FlappyBird
+//
+//  Created by Takuro Yamaguchi on 2016/05/11.
+//
+//
+
+#ifndef CharacterReader_hpp
+#define CharacterReader_hpp
+
+#include <stdio.h>
+
+#endif /* CharacterReader_hpp */
