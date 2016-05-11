5a0fcc20c3319b2144bc01640aacb77265c8770a
Takuro Yamaguchi <takuro.yamaguchi@PK-22110116.local>
2016-05-11 18:22:52 +0900

    First Commit

---
+++ proj.ios_mac/build/FlappyBird.build/Debug/FlappyBird-desktop.build/Objects-normal/x86_64/AppDelegate.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/takuro.yamaguchi/Documents/CocosProjects/FlappyBird/Classes/AppDelegate.cpp \
+  /Users/takuro.yamaguchi/Documents/CocosProjects/FlappyBird/Classes/AppDelegate.h \
+  /Users/takuro.yamaguchi/Documents/CocosProjects/FlappyBird/Classes/HelloWorldScene.h
---
+++ proj.ios_mac/build/FlappyBird.build/Debug/FlappyBird-desktop.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/takuro.yamaguchi/Documents/CocosProjects/FlappyBird/proj.ios_mac/mac/main.cpp \
+  /Users/takuro.yamaguchi/Documents/CocosProjects/FlappyBird/Classes/AppDelegate.h
